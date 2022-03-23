7e96584f0575b5dc8b0789b47026db24ab3bf53d
Hara <ecohumankaisei@gmail.com>
2022-03-23 01:26:00 +0000

    laravel03 task modiffy

--- laravel03.php
+++ laravel03.php
@@ -37,15 +37,22 @@ foreach ($fruits as $value) {
 ?>
 
 <?php
-    
 
-for($i = 1; $i <= 100; $i++) {
-    
-if($i % 5 == 0) 
-echo $i;
+// for文の始めの値を定義する 
+$start = 1;
+// for文の終わりの値を定義する 
+$end = 100;
 
+for($i = $start; $i <= $end; $i++) {
+
+// 5で割り切れたら{}内を実行する
+  if ($i % 5 == 0){
+    
+    echo $i;
+    echo "\n";
+  }
 }
-  
 
 ?>
+
 
